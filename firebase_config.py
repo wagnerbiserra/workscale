06c1ad16192d49ab98aae1d79c158c7899131f26
wagnerbiserra <wagnerbiserra@gmail.com>
2026-04-14 16:56:01 -0300

    use streamlit secrets for firebase

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,9 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Evita inicializar mais de uma vez
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase-key.json")
+    cred = credentials.Certificate(dict(st.secrets["firebase"]))
     firebase_admin.initialize_app(cred)
 
-# Conexão com Firestore
 db = firestore.client()
